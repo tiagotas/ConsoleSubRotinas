e6ff9edf18a3cac9f81fed7d36e773270d5fbd7b
TIAGO SILVA <frost.tiago@gmail.com>
2021-06-10 22:36:54 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleSubRotinas/Calculadora.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSubRotinas
+{
+    public class Calculadora
+    {
+        /**
+         * Assinatura da subrotina somar
+         * Com dois parâmetros do tipo double, n1 e n2
+         */ 
+        public static double somar(double n1, double n2)
+        {
+            // Variável local res, que recebe a soma dos dois parâmetros
+            // descritos na assinatura da subrotina.
+            double res = n1 + n2;
+
+            // Retorno da subrotina, tipo do retorno precisa "bater"
+            // com o tipo de retorno descrito na assinatura.
+            return res;
+        }
+
+        public static double subtrair(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+
+
+        public static double multiplicar(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+
+
+
+        
+    }
+}
---
+++ ConsoleSubRotinas/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleSubRotinas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Saldacao();
+
+            List<string> alunos = new List<string>();
+            alunos.Add("Lucas");
+            alunos.Add("Marcelo");
+            alunos.Add("Thiago");
+            alunos.Add("Tadeu");
+
+            MostraNomes(alunos);
+
+            getAlunoByPosition(alunos, 2);
+
+
+
+
+
+            Console.WriteLine("Calculadora com Subrotinas");
+
+            Console.WriteLine("Vamos Somar");
+
+
+            Console.WriteLine("Digite o primero número");
+            double digitado_n1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número");
+            double digitado_n2 = Convert.ToDouble(Console.ReadLine());
+
+
+
+            double retorno = Calculadora.somar(digitado_n1, digitado_n2);
+            Console.WriteLine("O resultado da soma é {0}", retorno);
+
+
+            Console.WriteLine(
+                "O resultado da subtração é {0}",
+                Calculadora.subtrair(digitado_n1, digitado_n2)
+            );
+
+
+            double retorno2 = Calculadora.multiplicar(4, 6.3);
+            Console.WriteLine("O resultado da 1ª multiplicação é {0}", retorno2);
+
+
+            double retorno3 = Calculadora.multiplicar(
+                Calculadora.somar(digitado_n1, digitado_n2),
+                2
+            );
+            Console.WriteLine("O resultado da 2ª multiplicação é {0}", retorno3);
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    
+        static void Saldacao()
+        {
+            Console.WriteLine("Bem-vindo ao estudo de subrotinas");
+        }
+
+
+        static void MostraNomes( List<string> lista_nomes )
+        {
+            foreach(string nome in lista_nomes)
+            {
+                Console.WriteLine("O nome da vez é {0}", nome);
+            }
+        }
+        
+        static void getAlunoByPosition( List<string> lista, int pos )
+        {
+            Console.WriteLine("O aluno da posição {0} é {1}", pos, lista[pos] );
+        }
+    }
+}
